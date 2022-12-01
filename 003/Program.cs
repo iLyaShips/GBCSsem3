cfbd9249c361cb278be9327a6179a491bd85e1c9
iLya <andr@google.com>
2022-12-01 17:06:10 +0300

    commit add 003

---
+++ 003/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+void Cube(int x)  // функция вывода квадратов чисел от 1 до N
+{
+    for (int i = 1; i <= x; i++)
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
+
+try
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Cube(num);
+}
+catch
+{
+    Console.WriteLine("Ошибка! Проверьте вводимое число.");
+}
